9649f227b1d1c50bc3f3fe8b3d2792428a6fdc37
kprgdv <k.prgdv.web@gmail.com>
2025-01-31 14:08:43 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,39 @@
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(pi);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0])
+
+//alert('Hello world')
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+// let answer = +prompt("Вам есть 18?", "Да");
+
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
